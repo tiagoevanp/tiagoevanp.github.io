3e78249597f4ab5114473eee1417893b122bbc99
Tiago Evangeslita Pinto <tiagoevanp@hotmail.com>
2025-10-26 23:17:43 -0300

    webpage V1

---
+++ _next/static/chunks/389a5d9ad8e5385a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/35ccb0de1e901dcf.js",
+  "static/chunks/d70f6a4ecb872a0f.js",
+  "static/chunks/turbopack-74e4d7517ecc859e.js"
+])
---
+++ _next/static/chunks/6aa3d682b726c06d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/2e119e84cf8f2339.js",
+  "static/chunks/d70f6a4ecb872a0f.js",
+  "static/chunks/turbopack-1a3da20f98adacef.js"
+])
