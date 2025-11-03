9d07e01cb478b89c6f4b39d0a161f3a2ee4e9959
Tiago Evangelista Pinto <tiagoevanp@hotmail.com>
2025-11-03 03:05:35 +0000

    update-19022668450

---
+++ _next/static/f-6NPivYVWQ5E-2bLrRXO/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/6aa3d682b726c06d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
