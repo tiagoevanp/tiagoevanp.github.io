4c5952a230e9b99d53f5b9522729e2f0fc43a01a
Tiago Evangelista Pinto <tiagoevanp@hotmail.com>
2025-11-21 23:31:49 +0000

    update-19586385980

---
+++ _next/static/chunks/7432fd9ba3df2274.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/b0d789579dababcf.js",
+  "static/chunks/3fee79a141c9ea0c.js",
+  "static/chunks/turbopack-7bb6e5ebc4e74354.js"
+])
---
+++ _next/static/chunks/f32df3172c95704e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/2900d3148ef6962d.js",
+  "static/chunks/3fee79a141c9ea0c.js",
+  "static/chunks/turbopack-ec609ebc2a2727b5.js"
+])
